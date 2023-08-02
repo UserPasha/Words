d6d8c41f2b5f515b21b4bdb1251501fcceadcf12
UserPasha <pasharoma13@gmail.com>
2023-08-02 13:39:24 +0300

    add packs covers, add layout to Pack

--- src/Match/Shop/Shop.tsx
+++ src/Match/Shop/Shop.tsx
@@ -4,6 +4,9 @@ import {BackArrow} from "../../Common/Components/BackArrow/BackArrow";
 import {useDispatch, useSelector} from "react-redux";
 import {AppDispatch, RootState} from "../../Store/store";
 import {Pack} from "../../Pack/Pack";
+import BronzeCoverPack from '../../assets/images/match/bg/BronzePackCover.jpg'
+import GoldPCoverPack from '../../assets/images/match/bg/GoldPackCover.jpg'
+import PlatinumCoverPack from '../../assets/images/match/bg/PlatinumPackCover.jpg'
 
 export const Shop = () => {
     const dispatch = useDispatch<AppDispatch>();
@@ -15,16 +18,37 @@ export const Shop = () => {
                 {currentPoints}
             </div>
            <div className={style.packsContainer}>
-               <Pack lowPercent={0.6} highPercent={0.9}/>
-               <Pack lowPercent={0.5} highPercent={0.85}/>
-               <Pack lowPercent={0.4} highPercent={0.8}/>
+               <Pack lowPercent={0.6}
+                     highPercent={0.9}
+                     cover={BronzeCoverPack}
+                     price={500}
+                     title={'Бронзовый набор'}
+                     bestLootInPercent={10}
+                     goodLootInPercent={30}
+                     badLootInPercent={60}
+               />
+               <Pack lowPercent={0.5}
+                     highPercent={0.85}
+                     cover={GoldPCoverPack}
+                     price={800}
+                     title={'Золотой набор'}
+                     bestLootInPercent={15}
+                     goodLootInPercent={35}
+                     badLootInPercent={50}
+               />
+               <Pack lowPercent={0.4}
+                     highPercent={0.8}
+                     cover={PlatinumCoverPack}
+                     price={1200}
+                     title={'Платиновый набор'}
+                     bestLootInPercent={20}
+                     goodLootInPercent={40}
+                     badLootInPercent={40}
+               />
 
 
            </div>
 
-            link to Pack (60 - 30 - 10) 500
-            link to Pack (50 - 35 - 15) 750
-            link to Pack (40 - 40 - 20) 1000
         </div>
     );
 };
--- src/Pack/Pack.tsx
+++ src/Pack/Pack.tsx
@@ -1,18 +1,37 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import style from './Pack.module.css'
 import okIcon from "../assets/images/match/ok.svg";
 import cancelIcon from "../assets/images/match/cancel.svg";
-import {useDispatch} from "react-redux";
-import {AppDispatch} from "../Store/store";
+import {useDispatch, useSelector} from "react-redux";
+import {AppDispatch, RootState} from "../Store/store";
 import {PointsAfterShop} from "../Store/profileReducer";
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import {PATH} from "../AppRoutes/AppRoutes";
 import {saveCurrentReward} from "../Store/RewardsReducer";
+
+
 interface IPack {
     lowPercent: number
     highPercent: number
+    cover: string
+    price: number
+    title: string
+    bestLootInPercent: number
+    goodLootInPercent: number
+    badLootInPercent: number
 }
-export const Pack:FC<IPack> = ({lowPercent, highPercent}) => {
+
+export const Pack: FC<IPack> = ({
+                                    lowPercent,
+                                    highPercent,
+                                    cover,
+                                    price,
+                                    title,
+                                    bestLootInPercent,
+                                    goodLootInPercent,
+                                    badLootInPercent
+                                }) => {
+
 
     const dispatch = useDispatch<AppDispatch>();
     const navigate = useNavigate();
@@ -20,19 +39,36 @@ export const Pack:FC<IPack> = ({lowPercent, highPercent}) => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
     const [isConfirmed, setConfirmed] = useState<boolean>(false)
 
+    const [isEnoughBalance, setIsEnoughBalance] = useState<boolean>(false)
+
+    const currentPoints = useSelector<RootState, number>(state => state.profile.currentPoints)
+
+    const checkBalance = () => {
+        setIsEnoughBalance(currentPoints < price);
+    };
+    useEffect(() => {
+        if (currentPoints < price) {
+            setIsEnoughBalance(true)
+        }
+    }, [isEnoughBalance])
+
     const handleOpenPack = () => {
         setConfirmed(true)
         GeneratePackDrop()
     };
 
-    const openPack = () => {
+    const openPack = (price: number) => {
+        checkBalance();
+        if (isEnoughBalance) {
+            return;
+        }
         setConfirmed(false)
-      //  dispatch(PointsAfterShop(1))
+        dispatch(PointsAfterShop(price))
         dispatch(saveCurrentReward(categoryLoot, brandLoot))
         setIsOpen(true);
         navigate(PATH.PACKOPENER)
     }
-    const cancelPack = () =>{
+    const cancelPack = () => {
         setConfirmed(false)
     }
     const rewardsData = [
@@ -61,36 +97,7 @@ export const Pack:FC<IPack> = ({lowPercent, highPercent}) => {
     const [categoryLoot, setCategoryLoot] = useState<string>("")
     const [brandLoot, setBrandLoot] = useState<string>("")
 
-    // const GeneratePackDrop = () => {
-    //     const randomNum = Math.random();
-    //     const categoryRanges = [
-    //         [0, 0.2],
-    //         [0.2, 0.4],
-    //         [0.4, 0.6],
-    //         [0.6, 0.8],
-    //         [0.8, 1],
-    //     ];
-    //     for (let i = 0; i < categoryRanges.length; i++) {
-    //         const [rangeStart, rangeEnd] = categoryRanges[i];
-    //         if (randomNum >= rangeStart && randomNum < rangeEnd) {
-    //             const secondRandomNum = Math.random();
-    //             const selectedCategory = rewardsData[i];
-    //             let selectedBrandIndex;
-    //             if (secondRandomNum < lowPercent) {
-    //                 selectedBrandIndex = 0;
-    //             } else if (secondRandomNum < highPercent) {
-    //                 selectedBrandIndex = 1;
-    //             } else if (secondRandomNum < newPercent) {
-    //                 selectedBrandIndex = 2;
-    //             } else {
-    //                 selectedBrandIndex = 3;
-    //             }
-    //             setCategoryLoot(selectedCategory.title);
-    //             setBrandLoot(selectedCategory.brands[selectedBrandIndex]);
-    //             break;
-    //         }
-    //     }
-    // };
+
     const GeneratePackDrop = () => {
         const randomNum = Math.random();
         const categoryRanges = [
@@ -117,38 +124,50 @@ export const Pack:FC<IPack> = ({lowPercent, highPercent}) => {
             }
         }
     };
+
+
     return (
         <div className={style.wrapper}>
 
             <div className={isOpen ? `${style.pack} ${style.open}` : `${''}`} onClick={() => setIsOpen(false)}>
 
             </div>
-            {/*{isOpen && <div className={style.shimmerEffect}>*/}
-
-            {/*</div>}*/}
             {isOpen && <div className={style.glowEffect}>
                 {categoryLoot}
                 {brandLoot}
             </div>}
             {isConfirmed && <div className={style.confirmModal}>
                 <div className={style.modalWindow}>
+                    <div className={style.packTitle}>
+                        {title}
+                    </div>
+                    <div className={style.packDescription}>
+                        <p>Проценты выпадения бонусов:</p>
+                        <p>Обычный: {badLootInPercent} %</p>
+                        <p>Хороший: {goodLootInPercent} %</p>
+                        <p>Отличный: {bestLootInPercent} %</p>
+                    </div>
                     <div className={style.message}>
-                        Вы хотите купить этот набор за 1 монет?
+                        Вы хотите купить этот набор за <span>{price}</span> монет?
                     </div>
                     <div className={style.buttons}>
                         <img src={okIcon} alt={'ok'}
-                             onClick={openPack}/>
+                             onClick={() => openPack(price)}/>
                         <img src={cancelIcon} alt={'cancel'}
                              onClick={cancelPack}/>
                     </div>
+
                 </div>
-
-
+                {isEnoughBalance && <div className={style.isNotEnoughCoins}>
+                    У вас недостаточно монет
+                </div>}
 
 
             </div>}
-            {!isOpen && <button className={style.openPackButton} onClick={handleOpenPack}>
-                Open Pack
+            {!isOpen && <button className={style.openPackButton}
+                                onClick={handleOpenPack}
+                                style={{backgroundImage: `url(${cover})`}}>
+
             </button>
             }
 
